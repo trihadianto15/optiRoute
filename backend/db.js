af7dbd3f96fd50ddb1b780db4209bce4e645f9a8
trihadianto15 <trihadianto15@gmail.com>
2026-06-30 22:20:28 +0700

    backend

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,18 @@
+require("dotenv").config();
+
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "route_optimization"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
   if (err) {
-    console.log(err);
+    console.error("Database connection failed:", err);
   } else {
     console.log("MySQL Connected");
   }
