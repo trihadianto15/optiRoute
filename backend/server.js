77748de47c17273714595f467e14dbecd5e95d8e
trihadianto15 <trihadianto15@gmail.com>
2026-06-30 22:30:14 +0700

    backend for render

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend Route Optimization is running");
+});
+
 /**
  * AMBIL SEMUA LOKASI
  */
@@ -125,8 +129,8 @@ app.get(
   }
 );
 
-app.listen(3001, () => {
-  console.log(
-    "Server running on port 3001"
-  );
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
